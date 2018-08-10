0d905a0e459135385876fa209cc39dc36ffa0935
yihec <1007087609@qq.com>
2018-08-10 13:57:50 +0800

    修改代码

--- server-1/src/main/java/com/cloud/cli/ServerController.java
+++ server-1/src/main/java/com/cloud/cli/ServerController.java
@@ -1,11 +1,13 @@
 package com.cloud.cli;
 
 import com.cloud.cli.service.FeignService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class ServerController {
@@ -14,6 +16,9 @@ public class ServerController {
     ServerService serverService;
     @Autowired
     FeignService feignService;
+    @Autowired
+    private RestTemplate restTemplate;
+
 
     //本地接口
     @GetMapping("/HelloServer1")
@@ -27,5 +32,17 @@ public class ServerController {
     public String HelloServer2(){
         logger.info("===<call trace-1>===");
         return feignService.HelloServer2("hi");
+    }
+
+    //调用了server-2的  HelloServer2接口
+    @GetMapping("/HelloServer3")
+    @HystrixCommand(fallbackMethod = "Error")
+    public String HelloServer3(){
+        logger.info("===<call trace-1>===");
+        return restTemplate.getForObject("http://localhost:2002/HelloServer2?name=hi", String.class);
+    }
+
+    public String Error() {
+        return "hi,系统错误";
     }
 }
