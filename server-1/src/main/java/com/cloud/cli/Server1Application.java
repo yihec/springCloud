044c889630809dc4424e0f33b865496e3d138b71
yihec <1007087609@qq.com>
2018-08-10 14:09:16 +0800

    修改代码

--- server-1/src/main/java/com/cloud/cli/Server1Application.java
+++ server-1/src/main/java/com/cloud/cli/Server1Application.java
@@ -5,7 +5,6 @@ import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServl
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
